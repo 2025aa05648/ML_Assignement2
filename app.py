4a86ad44b165319371effef2c3e383fcc9cd086e
2025aa05648 <2025aa05648@wilp.bits-pilani.ac.in>
2026-02-15 16:25:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if uploaded_file:
    # X = df.drop("target", axis=1)
     X = df.copy()
    # y = df["target"]
-     if "Loan_Status" in df.columns:
+    if "Loan_Status" in df.columns:
         df = df.drop("Loan_Status", axis=1)
 
     # Model selection
